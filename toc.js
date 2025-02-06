69a45e345f826b64c4b2103e07bd16ad5e317973
purplepalmdash <purplepalmdash@users.noreply.github.com>
2025-02-06 09:26:11 +0000

    deploy: aed01865651ce7b6bec87d2c72f069c88aa9ec54

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="year2025.html"><strong aria-hidden="true">1.</strong> Year2025</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="year2025.html"><strong aria-hidden="true">1.</strong> Year2025</a></li><li class="chapter-item expanded "><a href="20250206.html"><strong aria-hidden="true">2.</strong> 20250206</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
