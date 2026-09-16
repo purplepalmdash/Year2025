b518dd954ddd30c396aa2a513bed3e7e6239313a
purplepalmdash <purplepalmdash@users.noreply.github.com>
2026-09-16 10:01:18 +0000

    deploy: 99eda2b58be9c5db30a3aeb004b757d31dc52d81

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8bba9389.js',
+                path_to_root + 'searchindex-53b32bc0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
